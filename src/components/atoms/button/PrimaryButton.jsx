d0648436a7c68772b5d709771fd728ff008b25e9
Kohta9521 <kohta0421kochi@gmail.com>
2023-06-26 10:50:35 +0900

    add atoms lesson about atomic design

---
+++ src/components/atoms/button/PrimaryButton.jsx
@@ -0,0 +1,16 @@
+import react from "react";
+import styled from "styled-components";
+import { BaseButton } from "./BaseButton";
+
+const PrimaryButton = (props) => {
+  const { children } = props;
+  return (
+    <SButton>{children}</SButton>
+  )
+}
+
+const SButton = styled(BaseButton)`
+  background-color: #40514e;
+`
+
+export default PrimaryButton;
