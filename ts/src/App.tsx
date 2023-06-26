55e576a14bb7046ddfd2a0736d57e7f1709d6162
Kohta9521 <kohta0421kochi@gmail.com>
2023-06-26 16:09:47 +0900

    add function

--- ts/src/App.tsx
+++ ts/src/App.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import './App.css';
+import Practice1 from './practuces/Practice1';
+import Practice2 from './practuces/Practice2';
 
 function App() {
   return (
     <div className="App">
       <h1>Ts react project</h1>
+      <Practice1 />
+      <br />
+      <Practice2 />
     </div>
   );
 }
