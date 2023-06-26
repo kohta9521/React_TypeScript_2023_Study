46eb1a4e4ee9601661913eb43154aa2ee4f38712
Kohta9521 <kohta0421kochi@gmail.com>
2023-06-26 16:38:56 +0900

    add new Todo components

--- ts2/src/App.tsx
+++ ts2/src/App.tsx
@@ -1,12 +1,15 @@
 import axios from 'axios';
 import React from 'react';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
 
+  const [todos, setTodos] = useState<any>([]);
+
   const onClickFetchData = () => {
     axios.get("https://jsonplaceholder.typicode.com/todos").then((res) => {
-      console.log(res);
+      setTodos(res.data);
     })
   }
 
@@ -15,6 +18,9 @@ function App() {
       <h1>Ts project</h1>
 
       <button onClick={onClickFetchData}>データ取得</button>
+    {todos.map((todo) => (
+      <p>{todo.title}</p>
+    ))}
     </div>
   );
 }
