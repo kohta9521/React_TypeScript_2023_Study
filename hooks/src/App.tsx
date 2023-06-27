c123611e91be93e24c470252dda89c785f77bdd6
Kohta9521 <kohta0421kochi@gmail.com>
2023-06-27 21:04:17 +0900

    add all files about ts hooks lesson

--- hooks/src/App.tsx
+++ hooks/src/App.tsx
@@ -1,6 +1,28 @@
 import React from 'react';
 import './App.css';
 
+
+const dummyBooks: BookToRead[] = [
+  {
+    id: 1,
+    title: "初めてのReact",
+    authors: "ダミー",
+    memo: "",
+  },
+  {
+    id: 1,
+    title: "React Hooks入門",
+    authors: "ダミー",
+    memo: "",
+  },
+  {
+    id: 3,
+    title: "実践Reactアプリケーション",
+    authors: "ダミー",
+    memo: "",
+  }
+]
+
 function App() {
   return (
     <div className="App">
---
+++ hooks/src/BookRow.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { BookToRead } from "./BookToRead";
+
+type BookRowProps = {
+  book: BookToRead;
+  onMemoChange: (id: number, memo: string) => void;
+  onDelete: (id: number) => void;
+};
+
