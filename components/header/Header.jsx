f2fa9c3a8ce1c393c442acdeff55057cbfa08d73
Kohta9521 <kohta0421kochi@gmail.com>
2023-07-26 16:35:18 +0900

    add menu pc content

--- components/header/Header.jsx
+++ components/header/Header.jsx
@@ -1,10 +1,65 @@
-import React from 'react'
+import React, { useState } from 'react'
+import styles from "./Header.module.css";
+import Link from "next/link"
+
 
 const Header = () => {
+  const [ openMenu, setOpenMenu ] = useState(false);
+  const menuFunction = () => {
+    setOpenMene(!openMenu);
+  }
   return (
-    <div>
-      sadfasd
-    </div>
+    <>
+      <header id='header' className={styles.header}>
+        <div className={styles.logo}>
+          <Link href="/" className={styles.logo}>
+            Kohta
+          </Link>
+        </div>
+
+        <nav>
+          <ul>
+            <li>
+              <Link href="/">
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link href="/">
+                About
+              </Link>
+            </li>
+            <li>
+              <Link href="/">
+                Service
+              </Link>
+            </li>
+            <li>
+              <Link href="/">
+                Price
+              </Link>
+            </li>
+            <li>
+              <Link href="/">
+                Member
+              </Link>
+            </li>
+            <li>
+              <Link href="/">
+                Contact
+              </Link>
+            </li>
+          </ul>
+        </nav>
+        <div className={styles.container}>
+          <div className={styles.humburger} onClick={() => menuFunction()}>
+            <span className={openMenu ? styles.open : undefined}></span>
+            <span className={openMenu ? styles.open : undefined}></span>
+            <p className={openMenu ? styles.open : undefined}>Menu</p>
+          </div>
+        </div>
+      </header>
+    </>
   )
 }
 
