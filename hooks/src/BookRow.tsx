906dbe16f21abbba43b63feeb1e9e829a5d7d0e5
Kohta9521 <kohta0421kochi@gmail.com>
2023-07-26 16:04:16 +0900

    remove not use file

--- hooks/src/BookRow.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { BookToRead } from "./BookToRead";
-
-type BookRowProps = {
-  book: BookToRead;
-  onMemoChange: (id: number, memo: string) => void;
-  onDelete: (id: number) => void;
-};
-
